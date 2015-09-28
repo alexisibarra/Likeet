74b738a0cd036da925a73ee84d7e3bbd5ebc4b68
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-09-28 11:05:02 -0430

    IMplementando la logica para registrar usuarios

--- frontend/survey/app/scripts/controllers/register.js
+++ frontend/survey/app/scripts/controllers/register.js
@@ -8,12 +8,20 @@
  * Controller of the likeetApp
  */
 angular.module('likeetApp')
-  .controller('RegisterCtrl', function ($scope, $state) {
+  .controller('RegisterCtrl', ['$scope', '$state', 'Restangular', 'userService', function ($scope, $state, Restangular, userService) {
     $scope.UserData = {};
 
     $scope.registerUser = function(){
-      console.log('Registered ' + $scope.UserData.email);
+      var newUser = {email: $scope.UserData.email};
+      var baseUsers = Restangular.all('users');
 
-      $state.go('survey');
+      $scope.User = userService;
+
+      baseUsers.post(newUser).then(function(user){
+        $scope.User['id'] = user.id;
+        $scope.User['email'] = user.email;
+
+        $state.go('survey');
+      });
     };
-  });
+  }]);
