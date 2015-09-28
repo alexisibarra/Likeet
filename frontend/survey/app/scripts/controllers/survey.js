461d4574618f497fa69cb3edb0db889ebc79961b
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-09-28 11:05:54 -0430

    Implementando la logica para los likes de los usuarios a los recursos

--- frontend/survey/app/scripts/controllers/survey.js
+++ frontend/survey/app/scripts/controllers/survey.js
@@ -8,19 +8,29 @@
  * Controller of the likeetApp
  */
 angular.module('likeetApp')
-  .controller('SurveyCtrl', ['$scope', '$state', 'dataService', 'Restangular', function ($scope, $state, dataService, Restangular) {
+  .controller('SurveyCtrl', ['$scope', '$state', 'Restangular', 'userService', function ($scope, $state, Restangular, userService) {
+      $scope.User = userService;
 
-    var baseResources = Restangular.all('randomresources');
-    baseResources.getList().then(function(resources){
-      $scope.resource = resources[0];
-    });
-
-    $scope.processResource = function(resourceId, decision ){
-      if (decision){
-        console.log("You liked resource: " + resourceId);
-      } else {
-        console.log("You disliked resource: " + resourceId);
+      if($scope.User.isNotSet()){
+        $state.go('register');
       }
-      $state.go($state.current, {}, {reload: true});
-    };
+
+      var baseResources = Restangular.all('randomresources');
+      baseResources.getList().then(function(resources){
+          $scope.resource = resources[0];
+      });
+
+      $scope.processResource = function(resourceId, decision ){
+        var info = {
+            resourceId: resourceId,
+            like: decision
+        };
+
+        Restangular
+          .one('users', $scope.User.id)
+          .post('process', info)
+          .then(function(data){
+              $state.go($state.current, {}, {reload: true});
+          });
+      };
   }]);
